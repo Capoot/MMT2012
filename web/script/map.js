5191848a1584b9bc9a528ddbd8ad5431ce7afca8
Dominik Eckelmann <deckelmann@gmail.com>
2012-04-26 22:46:03 +0200

    changed infowindow

--- web/script/map.js
+++ web/script/map.js
@@ -24,7 +24,7 @@ var App = function() {
     this.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     this.showUserOnMap();
     this.addCamera(123);
-}
+};
 
 App.prototype.showUserOnMap = function() {
     var self = this;
@@ -34,35 +34,62 @@ App.prototype.showUserOnMap = function() {
     navigator.geolocation.getCurrentPosition(function(position) {
         self.showPosition(position.coords.latitude, position.coords.longitude);
     });
-}
+};
 
 App.prototype.showPosition = function(lat, lng) {
     var latlng = new google.maps.LatLng(lat, lng);
     this.userLocation = latlng;
     this.map.setCenter(latlng);
     this.map.setZoom(12);
-}
+};
 
 App.prototype.addCamera = function(id) {
+
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(52, 13),
         title:"Camera ###",
         map: this.map,
         icon: "img/video.png"
     });
-    google.maps.event.addListener(marker, 'click', function() {
-            var info = '<article><h1>LiveStream</h1>'
-                + '<video autoplay="autoplay" width="270" height="480" controls="controls">'
-                + '<source src="testData/video.webm" type="video/webm" />'
-                + '<source src="testData/video.mp4" type="video/mp4" />'
-                + '<source src="testData/video.webm" type="video/ogg; codecs=\'theora, vobis\'" />'
-                + '</video></article>';
-        var infowindow = new google.maps.InfoWindow({
-            content: info,
-            maxWidth: 500
-        });
-        infowindow.open(App.map, marker);
-        this.infoWindow = infowindow;
-    });
+
+    google.maps.event.addListener(marker, 'click', this.openInfoWindow);
+
     this.cameras[id] = marker;
-}
+};
+
+App.prototype.openInfoWindow = function() {
+    var info = '<h1>LiveStream</h1>'
+        + '<video autoplay="autoplay">'
+        + '<source src="testData/video.webm" type="video/webm" />'
+        + '<source src="testData/video.mp4" type="video/mp4" />'
+        + '<source src="testData/video.webm" type="video/ogg; codecs=\'theora, vobis\'" />'
+        + '</video>'
+        + '<button class="videoClose">close</button>';
+
+    var myOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(52, 13),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        scaleControl: false,
+        zoomControl: false,
+        panControl: false
+    };
+
+    var map = this.map;
+    map.setOptions(myOptions);
+
+    infoWindow = $('#infoWindow');
+    infoWindow.html(info);
+    infoWindow.removeClass('top');
+    infoWindow.addClass('right');
+    infoWindow.show();
+    $('.videoClose').click(function() {
+        infoWindow.hide();
+
+    });
+};
+
+App.prototype.closeWindow = function() {
+    this.infoWindow.close();
+};
